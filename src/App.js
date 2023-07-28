a248d9061bded55c63e1fc3f80ffbf913f611865
DalerKhusainov <dalerkhusainov85@gmail.com>
2023-07-28 16:32:02 +0500

    Started website for real estate agency

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// COMPONENTS
+import Sidebar from "../src/components/Sidebar";
+
+// STYLES
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
